b32fcf705b47bce119dc6315e95897ff4f263f70
ale1201 <ma.pabon@uniandes.edu.co>
2021-09-24 14:53:06 -0500

    visualización de eventos en lista

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const URL = "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json"
+const url = "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json"
 
-fetch(URL).then(res=>res.json()).then(res=>{
-    console.log("respuesta", res)
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('eventos');
+
+fetch(url).then((resp) => resp.json()).then(function(data) {
+    console.log("respuesta", data)
+  let eventos = data;
+  return eventos.map(function(event) {
+    let li = createNode('li');
+    let span = createNode('span');
+    span.innerHTML = `${event.events}`;
+    append(li, span);
+    append(ul, li);
+  })
 })
+.catch(function(error) {
+  console.log(error);
+});
